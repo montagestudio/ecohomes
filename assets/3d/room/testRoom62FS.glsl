376baf114220f09db12166d1aa1c2ced0f12faf6
Fabrice Robinet <fabricerobinet@me.com>
2013-08-08 14:23:09 -0700

    add newest version and update viewpoint

--- assets/3d/room/testRoom62FS.glsl
+++ assets/3d/room/testRoom62FS.glsl
@@ -4,19 +4,16 @@ uniform vec3 u_light0Color;
 varying vec2 v_texcoord0;
 uniform sampler2D u_ambient;
 uniform sampler2D u_diffuse;
+uniform vec4 u_emission;
 uniform vec4 u_specular;
 uniform vec4 u_filterColor;
 uniform float u_transparency;
 void main(void) {
+vec3 normal = normalize(v_normal);
 vec4 color = vec4(0., 0., 0., 0.);
 vec4 diffuse = vec4(0., 0., 0., 1.);
 vec3 diffuseLight = vec3(0., 0., 0.);
-vec4 ambient;
-vec3 ambientLight = vec3(0., 0., 0.);
-vec4 specular;
-vec3 specularLight = vec3(0., 0., 0.);
 diffuse = texture2D(u_diffuse, v_texcoord0);
 color.xyz += diffuse.xyz;
-color *= u_filterColor;
 gl_FragColor = vec4(color.rgb * diffuse.a, diffuse.a * u_transparency);
 }
