dd27309716dd57f753a82752c83a756cbf3381ae
António Afonso <antonio.afonso@gmail.com>
2013-08-15 18:09:46 -0700

    Remove console.log

--- ui/card.reel/card.js
+++ ui/card.reel/card.js
@@ -48,7 +48,6 @@ exports.Card = Component.specialize(/** @lends Card# */ {
             // measure the container.
             if (!this._hasResize) {
                 if (this.element.offsetParent) {
-                    console.log(this.element.offsetParent);
                     this._hasResize = true;
                     this._width = this.element.offsetParent.offsetWidth;
                     this._height = this.element.offsetParent.offsetHeight;
