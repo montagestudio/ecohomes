79a35251477669aff74db90b765988d90be85ec7
António Afonso <antonio.afonso@gmail.com>
2013-08-17 21:40:56 -0700

    Add ChoiceOptionField

---
+++ ui/option-fields/choice-option-field.reel/choice-option-field.js
@@ -0,0 +1,17 @@
+/**
+ * @module ./choice-option-field.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ChoiceOptionField
+ * @extends Component
+ */
+exports.ChoiceOptionField = Component.specialize(/** @lends ChoiceOptionField# */ {
+    constructor: {
+        value: function ChoiceOptionField() {
+            this.super();
+        }
+    }
+});
