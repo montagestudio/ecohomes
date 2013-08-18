8e1c0181ea8536d386bd448c16567d1125ce9eac
António Afonso <antonio.afonso@gmail.com>
2013-08-18 13:23:06 -0700

    Add RangeOptionField

---
+++ ui/option-fields/range-option-field.reel/range-option-field.js
@@ -0,0 +1,17 @@
+/**
+ * @module ./range-option-field.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class RangeOptionField
+ * @extends Component
+ */
+exports.RangeOptionField = Component.specialize(/** @lends RangeOptionField# */ {
+    constructor: {
+        value: function RangeOptionField() {
+            this.super();
+        }
+    }
+});
