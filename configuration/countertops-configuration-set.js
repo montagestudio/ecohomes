3e6714df045191a0ac890560573c8541d6fbb363
Mike Czepiel <mike@czepiel.net>
2013-08-19 14:55:22 -0700

    Add countertop options
    Need to add textures for these and wire that up next

--- configuration/countertops-configuration-set.js
+++ configuration/countertops-configuration-set.js
@@ -1,11 +1,21 @@
 var ConfigurationSet = require("core/configuration-set").ConfigurationSet;
 var ConfigurationOption = require("core/configuration-option").ConfigurationOption;
+var ConfigurationChoice = require("core/configuration-choice").ConfigurationChoice;
 
 exports.CountertopsConfigurationSet = ConfigurationSet.specialize({
 
     constructor: {
         value: function CountertopsConfigurationSet() {
             this.super();
+
+            var optionMap = this.optionMap;
+            optionMap.set("material", new ConfigurationChoice().init(
+                new ConfigurationOption().init("Bamboo", 3299),
+                new ConfigurationOption().init("Paper Composite", 1999),
+                new ConfigurationOption().init("Cement and Fly Ash", 1599),
+                new ConfigurationOption().init("Black Quartz", 0)
+            ));
+
         }
     }
 
