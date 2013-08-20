d7015bee2c4fe913cb165b28377a6b384e9789b9
Michael Koch <mikokim@gmail.com>
2013-08-20 11:16:44 -0700

    edit countertop pricing

--- configuration/countertops-configuration-set.js
+++ configuration/countertops-configuration-set.js
@@ -10,10 +10,10 @@ exports.CountertopsConfigurationSet = ConfigurationSet.specialize({
 
             var optionMap = this.optionMap;
             optionMap.set("material", new ConfigurationChoice().init(
-                new ConfigurationOption().init("Bamboo", 3299),
-                new ConfigurationOption().init("Paper Composite", 1999),
-                new ConfigurationOption().init("Cement and Fly Ash", 1599),
-                new ConfigurationOption().init("Black Quartz", 0)
+                new ConfigurationOption().init("Black Quartz", 5400),
+                new ConfigurationOption().init("Cement and Fly Ash", 4599),
+                new ConfigurationOption().init("Bamboo", 3199),
+                new ConfigurationOption().init("Paper Composite", 2799)
             ));
 
         }
