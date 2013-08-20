c914d496fa70de60b5de0aa6fab9dc3fde66e352
António Afonso <antonio.afonso@gmail.com>
2013-08-20 12:07:02 -0700

    Add Summary component

---
+++ ui/summary.reel/summary.js
@@ -0,0 +1,17 @@
+/**
+ * @module ./summary.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Summary
+ * @extends Component
+ */
+exports.Summary = Component.specialize(/** @lends Summary# */ {
+    constructor: {
+        value: function Summary() {
+            this.super();
+        }
+    }
+});
