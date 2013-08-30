465303c5592f36e0d24ad0da5989f63f8a75a7df
François Frisch <francoisfrisch@gmail.com>
2013-08-30 17:42:41 +0200

    Use env variables for production credentials.

--- server/config/production.js
+++ server/config/production.js
@@ -1,8 +1,8 @@
-var credentials = require("./credentials.js");
+
 
 module.exports = {
-    accountSid: credentials.twilio.accountSid,
-    authToken: credentials.twilio.authToken,
+    accountSid: process.env.TWILIO_ACCOUNT_SID,
+    authToken: process.env.TWILIO_AUTH_TOKEN,
     fromNumber: "+14084268362",
     appEntryUrl: "http://audition.montagejs.org/api/initiateCall"
 };
