206d3b3c867eb39b750519df35a667c900267012
Fabrice Robinet <fabricerobinet@me.com>
2013-09-04 16:57:12 -0700

    Added cube map to the scene.

--- assets/3d/loft4/loft42FS.glsl
+++ assets/3d/loft4/loft42FS.glsl
@@ -1,20 +1,22 @@
 precision highp float;
 varying vec3 v_normal;
 varying vec2 v_texcoord0;
+varying vec3 v_reflect;
 uniform sampler2D u_diffuse;
-varying vec2 v_texcoord1;
-uniform sampler2D u_reflective;
-uniform float u_transparency;
+uniform sampler2D u_specular;
+uniform samplerCube u_reflective;
 uniform vec4 u_filterColor;
 void main(void) {
 vec4 color = vec4(0., 0., 0., 0.);
 vec4 diffuse = vec4(0., 0., 0., 1.);
-diffuse = texture2D(u_diffuse, v_texcoord0);
-
 vec4 reflective;
-reflective = texture2D(u_reflective, v_texcoord1);
-//diffuse.xyz += reflective.xyz;
+
+vec4 specular = texture2D(u_specular, v_texcoord0);
+diffuse = texture2D(u_diffuse, v_texcoord0);
+reflective = textureCube(u_reflective, v_reflect);
+
+diffuse.xyz += reflective.xyz * reflective.xyz * 0.2;
 color.xyz += diffuse.xyz;
-color = vec4(color.rgb * diffuse.a, diffuse.a * u_transparency);
+	color = vec4(color.rgb * diffuse.a, diffuse.a);
 gl_FragColor = color;
 }
--- assets/3d/loft4/loft42VS.glsl
+++ assets/3d/loft4/loft42VS.glsl
@@ -7,15 +7,12 @@ uniform mat4 u_worldViewMatrix;
 uniform mat4 u_projectionMatrix;
 attribute vec2 a_texcoord0;
 varying vec2 v_texcoord0;
-varying vec2 v_texcoord1;
+varying vec3 v_reflect;
 void main(void) {
 vec4 pos = u_worldViewMatrix * vec4(a_position,1.0);
 v_normal = normalize(u_normalMatrix * a_normal);
 v_texcoord0 = a_texcoord0;
-vec3 normalizedVert = normalize(vec3(pos));
-vec3 r = reflect(normalizedVert, v_normal);
-r.z += 1.0;
-float m = 2.0 * sqrt(dot(r,r));
-v_texcoord1 = (r.xy / m) + 0.5;
+vec3 normalizedVert = normalize(-vec3(pos));
+v_reflect = reflect(normalizedVert, v_normal);
 gl_Position = u_projectionMatrix * pos;
 }
