973752ba865d4479da5439463166c53463e0f294
Mike Czepiel <mike@czepiel.net>
2013-09-10 21:21:58 -0700

    Dismiss navigation upon interaction outside
    Now it gets out of the way when you're done with it as you'd expect

--- ui/navigation.reel/navigation.js
+++ ui/navigation.reel/navigation.js
@@ -27,9 +27,53 @@ exports.Navigation = Component.specialize(/** @lends Navigation# */ {
         value: null
     },
 
+    _isShown: {
+        value: false
+    },
+
+    isShown: {
+        get: function () {
+            return this._isShown;
+        },
+        set: function (value) {
+            if (value === this._isShown) {
+                return;
+            }
+
+            this._isShown = value;
+
+            if (value) {
+                this.templateObjects.dismissPressComposer.load();
+            } else {
+                this.templateObjects.dismissPressComposer.unload();
+            }
+        }
+    },
+
+    handlePressStart: {
+        value: function (evt) {
+            if (this.isShown) {
+                var targetElement = evt.targetElement,
+                    element = this.element;
+
+                if (!element.contains(targetElement)) {
+                    this.isShown = false;
+                }
+            }
+        }
+    },
+
     handleAction: {
         value: function(event) {
             this.currentPanelIndex = event.detail.get("index");
+        }
+    },
+
+    enterDocument: {
+        value: function (firstTime) {
+            if (firstTime) {
+                this.addComposerForElement(this.templateObjects.dismissPressComposer, this.element.ownerDocument);
+            }
         }
     }
 });
