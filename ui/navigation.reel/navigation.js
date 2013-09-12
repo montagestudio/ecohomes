01d8d4f521a668891d1a4f8ee09c705a5bd763d6
Mike Czepiel <mike@czepiel.net>
2013-09-12 01:03:49 -0700

    Fix nav dismissal pressComposer with touch
    This updates montage to a quick fix such that a pressComposer correctly
    ends interaction by removing the eventListeners it installed when the
    interaction started.
    
    More importantly it cancelsPress on the pressComposer after pressStart,
    freeing other pressComposers further down the line to claim the now
    surrendered pointer.
    
    Also, there are some side-effects to the pressComposer being declared
    in the template (it loads) which we now avoid by creating it
    programmatically and unloading it early and often.

--- ui/navigation.reel/navigation.js
+++ ui/navigation.reel/navigation.js
@@ -2,16 +2,23 @@
  * @module ./navigation.reel
  * @requires montage/ui/component
  */
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    PressComposer = require("montage/composer/press-composer").PressComposer;
 
 /**
  * @class Navigation
  * @extends Component
  */
 exports.Navigation = Component.specialize(/** @lends Navigation# */ {
+
+    _dismissPressComposer: {
+        value: null
+    },
+
     constructor: {
         value: function Navigation() {
             this.super();
+            this._dismissPressComposer = new PressComposer();
         }
     },
 
@@ -43,9 +50,9 @@ exports.Navigation = Component.specialize(/** @lends Navigation# */ {
             this._isShown = value;
 
             if (value) {
-                this.templateObjects.dismissPressComposer.load();
+                this._dismissPressComposer.load();
             } else {
-                this.templateObjects.dismissPressComposer.unload();
+                this._dismissPressComposer.unload();
             }
         }
     },
@@ -59,6 +66,17 @@ exports.Navigation = Component.specialize(/** @lends Navigation# */ {
                 if (!element.contains(targetElement)) {
                     this.isShown = false;
                 }
+
+                // Regardless of whether we dismiss the navigation or not,
+                // surrender the pointer so the interaction can continue as
+                // if this composer was never here.
+                // We could also do this only if the press was inside the nav
+                // if we want to have to press to dismiss without allowing the
+                // interaction to continue.
+                // We need to do this at least then to allow the pressComposers
+                // inside the nav to continue to work as they try to claim the
+                // pointer to continue on their way to dispatch a press event
+                this._dismissPressComposer.cancelPress();
             }
         }
     },
@@ -72,7 +90,12 @@ exports.Navigation = Component.specialize(/** @lends Navigation# */ {
     enterDocument: {
         value: function (firstTime) {
             if (firstTime) {
-                this.addComposerForElement(this.templateObjects.dismissPressComposer, this.element.ownerDocument);
+                this.addComposerForElement(this._dismissPressComposer, this.element.ownerDocument);
+                this._dismissPressComposer.addEventListener("pressStart", this, false);
+
+                if (!this.isShown) {
+                    this._dismissPressComposer.unload();
+                }
             }
         }
     }
